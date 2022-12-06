a5212024aee70dbb042776b7a588fc02c8dbdfa1
Celso Luis Botelho Brito Junior <celso.botelho@solus-it.local>
2022-12-06 17:19:33 -0300

    feat:Funções de validação de dados do input

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,65 @@ function sendData(){
     let email = document.getElementById('email').value;
 
 
+    function validateCPNJ(documentNumber){
+        const validationNumbers = [ 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 ]
+        const cnpj = String(documentNumber).replace(/[^\d]/g, '')
+        
+
+        if(cnpj.length !== 14){
+            return false
+        }
+    
+
+        if(/0{14}/.test(cnpj)){
+            return false
+        }
+    
+
+        for(let i = 0, n = 0; i < 12; n += cnpj[i] * validationNumbers[++i]){
+            if(cnpj[12] != (((n %= 11) < 2) ? 0 : 11 - n)){
+                return false
+            }
+        }
+    
+
+        for (let i = 0, n = 0; i <= 12; n += cnpj[i] * validationNumbers[i++]){
+            if(cnpj[13] != (((n %= 11) < 2) ? 0 : 11 - n)){
+                return false
+            }
+        }
+
+
+        return true
+    }
+
+
+    function validateEmail(emailField){
+        let regex = new RegExp(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/);
+        return regex.test(emailField);
+    }
+
+
+    function validatePhone(phoneField){
+        let regex = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
+        return regex.test(phone);
+    }
+    
+
+    if(!validateCPNJ(document)){
+        //tratamento de erro na tela
+    }
+
+
+    if(!validateEmail(email)){
+        //tratamento de erro na tela
+    }
+
+
+    if(!validatePhone(phone)){
+        //tratamento de erro na tela
+    }
+
     const raw = JSON.stringify({
         name,
         email,
@@ -36,7 +95,7 @@ function sendData(){
         return response.json();
     })
     .then(result => {
-
+        //Tratamento de sucesso ao enviar o form
     })
     .catch(error => console.log('error', error));
 }
